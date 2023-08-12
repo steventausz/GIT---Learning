47e8720a98622ad0070b18b0bc132db914ef2b96
Steven Tausz <steventausz@gmail.com>
2023-08-12 18:06:54 -0500

    Bootstrap react

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const path = require('path');
 const PORT = 3000;
 const app = express();
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,"public","index.html"))
+})
+
 
 
 
